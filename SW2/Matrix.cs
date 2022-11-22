0d9a816de1fe74463199f2f34bc97e5dd12a6c9e
Chedser <chedser@gmail.com>
2022-11-22 23:56:53 +0300

    Add files via upload

---
+++ SW2/Matrix.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+
+namespace StudyWork2
+{
+    class Matrix
+    {
+        int _rows;                  // Количество строк
+        int _cols;                  // Количество столбцов
+        int[] _dim = new int[2];    //Размерность
+        int[,] _matrix;     // Матрица
+        int _zero_elements_count;
+
+        public Matrix(int rows, int cols, int max_value = 4)          //Конструктор
+        {
+            GenerateMatrix(rows, cols, max_value);
+
+        }
+
+        /* Свойства */
+        public int Rows { get { return _rows; } }
+        public int Cols { get { return _cols; } }
+        public string Dim { get { return "[" + _dim[0] + "," + _dim[1] + "]"; } }
+        public int ZeroElementsCount { get { return _zero_elements_count; } }
+      
+        /* Печать матрицы */
+        public void Print()
+        {
+            foreach (int i in Enumerable.Range(0, _rows))
+            {
+                foreach (int j in Enumerable.Range(0, _cols))
+                {
+                    Console.Write(_matrix[i, j] + " ");
+                }
+                Console.WriteLine("\n");
+
+            }
+        }
+
+        /* Установка значения матрицы */
+        public void SetValue(int i, int j, int value)
+        {
+            _matrix[i, j] = value;
+        }
+
+        /* Получить значение матрицы */
+        public int GetValue(int i, int j)
+        {
+            return _matrix[i, j];
+        }
+
+        /* Сложение матриц */
+        public Matrix Add(ref Matrix sm)
+        {
+
+            if ((sm.Rows != this.Rows) || (sm.Cols != this.Cols))
+            {
+                throw new Exception();
+            }
+
+            Matrix new_matrix = new Matrix(sm.Rows, sm.Cols);
+
+            int current_element = 0;
+
+            foreach (int i in Enumerable.Range(0, this.Rows))
+            {
+                foreach (int j in Enumerable.Range(0, this.Cols))
+                {
+                    current_element = this.GetValue(i, j) + sm.GetValue(i, j);
+
+                    new_matrix.SetValue(i, j, current_element);
+                }
+            }
+
+            return new_matrix;
+
+        }
+
+        /* Создание разреженной матрицы */
+        public ArrayList Sparse() {
+
+            ArrayList A = new ArrayList(); // Массив ненулевых значений
+            ArrayList LJ = new ArrayList(); // Массив номеров столбцов ненулевых значений
+            ArrayList LI = new ArrayList(); // Массив количества ненулевых значений
+
+            int NNZ = 0; //Количество ненулевых элементов в текущей строке
+
+            for (int i = 0; i < _rows; i++)
+            {
+                for (int j = 0; j < _cols; j++)
+                {
+                    if (_matrix[i, j] != 0)
+                    {
+                        A.Add(_matrix[i, j]); // Добавить ненулевой элемент
+                        LJ.Add(j); //Добавить номер столбца
+                        NNZ++; // Увеличить количество ненулевых элементов
+                    }
+                }
+                LI.Add(NNZ); //Добавить количество ненулевых элементов
+            }
+
+            ArrayList total = new ArrayList(); // Результирующая матрица
+
+            total.Add(A);
+            total.Add(LJ);
+            total.Add(LI);
+
+            return total;
+
+        }
+
+        /* Генерация матрицы */
+        private void GenerateMatrix(int rows, int cols, int max_value = 4)
+        {
+            _rows = rows;
+            _cols = cols;
+            _dim[0] = _rows;
+            _dim[1] = _cols;
+
+            _matrix = new int[rows, cols];
+
+            foreach (int i in Enumerable.Range(0, rows))
+            {
+                foreach (int j in Enumerable.Range(0, cols))
+                {
+                    _matrix[i, j] = new Random().Next(0, max_value + 1);
+                }
+            }
+
+            SetZeroElementsCount();
+
+        }
+
+        private void SetZeroElementsCount() {
+
+            foreach (int i in Enumerable.Range(0, _rows))
+            {
+                foreach (int j in Enumerable.Range(0, _cols))
+                {
+                    if (_matrix[i, j] == 0) { ++_zero_elements_count; };
+                }
+            }
+
+        }
+
+    }
+}
---
+++ SW2/StudyWork2.cs
@@ -0,0 +1,158 @@
+﻿/*
+StudyWork2.cpp – Листинг программы для 2-ой лабораторной работы
+* Вариант 2
+*******************************************************************
+* Имя файла : StudyWork2.cpp
+* Резюме : Программа написанная на языке программирования С#
+* Описание :  Даны две разреженные матрицы (Ленточный строчный формат). 
+* Сложить их и результат занести в разреженную матрицу CS
+******************************************************************
+*/
+using System;
+using System.Collections;
+
+namespace StudyWork2
+{
+    class StudyWork2
+    {
+
+        static void Main(string[] args)
+        {
+
+            /* Количество строк и столбцов */
+            int rows1 = 0;
+            int cols1 = 0;
+            int rows2 = 0;
+            int cols2 = 0;
+
+            /* Переменные для валидации ввода */
+            bool is_valid_input = false;
+            bool is_valid_rows1 = false;
+            bool is_valid_cols1 = false;
+            bool is_valid_rows2 = false;
+            bool is_valid_cols2 = false;
+
+            Console.WriteLine("First matrix");
+
+            /* Валидация ввода */
+            while (!is_valid_input) {
+
+                try {
+
+                    if (!is_valid_rows1) {
+                        Console.WriteLine("Enter count of rows");
+                        rows1 = Convert.ToInt32(Console.ReadLine());
+
+                        if (rows1 <= 0)
+                        {
+                            Console.WriteLine("Wrong input");
+                            continue;
+                        }
+
+                    }
+
+                    is_valid_rows1 = true;
+
+                    if (!is_valid_cols1)
+                    {
+
+                        Console.WriteLine("Enter count of cols");
+
+                        cols1 = Convert.ToInt32(Console.ReadLine());
+
+                        if (cols1 <= 0)
+                        {
+                            Console.WriteLine("Wrong input");
+                            continue;
+                        }
+
+                    }
+
+                    is_valid_cols1 = true;
+
+                    if (!is_valid_rows2) {
+
+                        Console.WriteLine("\nSecond matrix");
+                        Console.WriteLine("Enter count of rows");
+                        rows2 = Convert.ToInt32(Console.ReadLine());
+
+                        if (rows2 <= 0)
+                        {
+                            Console.WriteLine("Wrong input");
+                            continue;
+                        }
+
+                        if (rows1 != rows2)
+                        {
+                            Console.WriteLine("Rows must be equals\n");
+                            continue;
+                        }
+
+                    }
+
+                    is_valid_rows2 = true;
+
+                    if (!is_valid_cols2)
+                    {
+
+                        Console.WriteLine("Enter count of cols");
+
+                        cols2 = Convert.ToInt32(Console.ReadLine());
+
+                        if (cols2 <= 0)
+                        {
+                            Console.WriteLine("Wrong input");
+                            continue;
+                        }
+
+                        if (cols1 != cols2)
+                        {
+                            Console.WriteLine("Cols must be equals\n");
+                            continue;
+                        }
+
+                    }
+
+                    is_valid_cols2 = true;
+                    is_valid_input = true;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("\nWrong input");
+                }
+
+            }
+
+            Matrix sm1 = new Matrix(rows1, cols1, 1);
+            Matrix sm2 = new Matrix(rows2, cols2, 1);
+
+            Console.WriteLine("\nMatrix1");
+            sm1.Print();
+
+            Console.WriteLine("\nMatrix2");
+            sm2.Print();
+
+            Matrix sm3 = sm1.Add(ref sm2);          // Сложение элементов матриц
+
+            Console.WriteLine("\nSum of matrixes");
+            sm3.Print();
+
+            ArrayList sparsedMatrix = sm3.Sparse(); // Разреженная матрица
+
+            Console.WriteLine("Sparsed matrix");
+            PrintSparsedMatrix(sparsedMatrix);      // Отображение разреженной матрицы
+
+        }
+
+        static void PrintSparsedMatrix(ArrayList matrix) {
+            foreach (ArrayList list in matrix) {
+                foreach (int element in list)
+                {
+                    Console.Write(element + " ");
+                }
+                Console.WriteLine("\n");
+            }
+        } 
+    }
+}
+
